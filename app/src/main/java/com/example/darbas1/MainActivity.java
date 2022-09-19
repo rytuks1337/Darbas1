a6066236904572f5822a0a79233ebb5868d1dfd1
rytuks1337 <rytisyrasertvytis02@gmail.com>
2022-09-19 15:33:05 +0300

    Added a button, that changes the color of text to red

--- app/src/main/java/com/example/darbas1/MainActivity.java
+++ app/src/main/java/com/example/darbas1/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.darbas1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,11 +17,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button btn = findViewById(R.id.buttonAppear);
+        Button btncolor = findViewById(R.id.buttonColor);
+        TextView text = findViewById(R.id.textView);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(getApplicationContext(), "The Button Has Been Clicked", Toast.LENGTH_LONG).show();
+            }
+        });
+        btncolor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text.setTextColor(Color.rgb(100,0,0));
             }
         });
 
